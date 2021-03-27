142beeb95a20a6367222864f1eefa9decee35d46
Bhavjot Singh <bhavjot794@gmail.com>
2021-03-28 00:18:32 +0530

    Navbar completed

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import "../styles.css"
 import {API} from "../backend"
+import Base from "./Base"
 function Home() {
     console.log("API IS",process.env.REACT_APP_BACKEND)
     return (
-        <div>
-            <h1 className="text-white">Hello Frontend {API}</h1>
-        </div>
+        <Base title="Homepage">
+            <div className="row">
+                <div className="col-4"><button className="btn btn-success">Test</button></div>
+                <div className="col-4"><button className="btn btn-success">Test</button></div>
+                <div className="col-4"><button className="btn btn-success">Test</button></div>
+            </div>
+        </Base>
     );
 }
 
